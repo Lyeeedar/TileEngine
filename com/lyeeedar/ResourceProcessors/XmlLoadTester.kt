d4ab90550677652049faf258fdbd4e9b20bfc7ae
lyeeedar <lyeeedar@gmail.com>
2020-01-02 06:05:10 +0000

    Add resource compilers to the engine

---
+++ com/lyeeedar/ResourceProcessors/XmlLoadTester.kt
@@ -0,0 +1,34 @@
+package com.lyeeedar.ResourceProcessors
+
+import com.lyeeedar.Renderables.Particle.ParticleEffect
+import com.lyeeedar.Renderables.Particle.ParticleEffectDescription
+import com.lyeeedar.Util.XmlData
+
+class XmlLoadTester
+{
+	companion object
+	{
+		fun test()
+		{
+			for (path in XmlData.getExistingPaths().toList())
+			{
+				try
+				{
+					val xml = XmlData.getXml(path)
+					when (xml.name.toUpperCase())
+					{
+						"EFFECT" -> ParticleEffect.load(path.split("Particles/")[1], ParticleEffectDescription(""))
+						else -> GameXmlLoadTester.testLoad(xml)
+					}
+
+					System.out.println("Test loaded '$path'")
+				}
+				catch (ex: Exception)
+				{
+					System.err.println("Failed to load '$path'")
+					throw ex
+				}
+			}
+		}
+	}
+}
